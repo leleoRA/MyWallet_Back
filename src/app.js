4c72090f70491910e605f887190011ad3871ddfa
Thiago Pereira <thiago.spereira02@gmail.com>
2021-06-22 21:10:17 -0300

    Post of login route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import pg from 'pg';
 import bcrypt from 'bcrypt';
 import joi from 'joi';
+import {v4 as uuidv4} from "uuid";
 
 const app = express();
 app.use(cors());
@@ -19,6 +20,8 @@ const databaseConfig = {
 const { Pool } = pg;
 const connection = new Pool(databaseConfig);
 
+/* ------- Sign-up ------- */
+
 app.post('/sign-up', async (req, res) => {
     const { name, email, password, confirmPassword } = req.body;
     const userSchema = joi.object({
@@ -31,7 +34,10 @@ app.post('/sign-up', async (req, res) => {
     try{
         if (password !== confirmPassword) return res.sendStatus(406);
 
-        const emailExists = await connection.query(`SELECT * FROM users WHERE email = $1`, [email]);
+        const emailExists = await connection.query(`
+            SELECT * FROM users 
+            WHERE email = $1`
+            , [email]);
         if (emailExists.rows[0]) return res.sendStatus(409);
 
         const hash = bcrypt.hashSync(password, 12);
@@ -53,6 +59,41 @@ app.post('/sign-up', async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+/* ------- Login ------- */
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const result = await connection.query(`
+            SELECT * FROM users
+            WHERE email = $1`
+            , [email]);
+        const user = result.rows[0];
+
+        if (user && bcrypt.compareSync(password, user.password)){
+            const token = uuidv4();
+
+            await connection.query(`
+                INSERT INTO sessions
+                ("userId", token)
+                VALUES ($1, $2)`
+                , [user.id, token]);
+
+            res.send(token)
+            console.log(token)
+
+        } else{
+            return res.sendStatus(401);
+        }
+
+    } catch (e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
 
 app.listen(4000, () => {
     console.log('Server running on port 4000')
